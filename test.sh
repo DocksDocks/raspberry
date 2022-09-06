a02106927bfcadcb09bc1f7c2d049bd0885548d3
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-06 16:01:19 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-if [ $1 -gt 4 ]; then
-    SWAP=$((1024*4)) 
+if [ $1 -eq 0 ]; 
+then
+  $SWAP = 0
+elif [ $1 -lt 0 ]; 
+then
+  $SWAP = 100
+elif [ $1 -gt 8 ];
+then
+  SWAP=$((1024*8)) 
 else
- SWAP=$(( 1024 * $1))
+  SWAP=$(( 1024 * $1))
 fi
-echo "Testing... swap size: $SWAP"
+
+
+if [ $SWAP -eq 0 ];
+then
+    echo "Was set to default"
+else
+    echo "Changed successfuly to ${SWAP}MB"
+ fi
