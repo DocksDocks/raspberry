99d6038b3da0b89a3a9150042935edd695c97db7
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-06 15:47:02 -0300

    Update limit_swap_change.sh

--- limit_swap_change.sh
+++ limit_swap_change.sh
@@ -1,12 +1,12 @@
-if [ $1 -gt 8 ]; 
+if [ "$1" == "default" ]; 
 then
-  SWAP=$((1024*8)) 
+  SWAP = 0
 elif [ $1 -lt 0 ]; 
 then
   SWAP=100
-elif [ "$1" == "default" ];
+elif [ $1 -gt 8 ];
 then
-    $SWAP = 0 
+  SWAP=$((1024*8)) 
 else
   SWAP=$(( 1024 * $1))
 fi
