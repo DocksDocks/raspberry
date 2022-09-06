3e87500da14be5fd85c7ea305a97dd6ed24523cb
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-06 16:12:48 -0300

    Update limit_swap_change.sh

--- limit_swap_change.sh
+++ limit_swap_change.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $1 -eq 0 ]; 
 then
-  SWAP = 0
+  $SWAP = 0
 elif [ $1 -lt 0 ]; 
 then
-  SWAP = 100
+  $SWAP = 100
 elif [ $1 -gt 8 ];
 then
   SWAP=$((1024*8)) 
@@ -13,7 +13,7 @@ else
 fi
 
 
-if [ SWAP -gt 0 ];
+if [ $SWAP -eq 0 ];
 then
     sudo dphys-swapfile swapoff && \
     printf "# /etc/dphys-swapfile - user settings for dphys-swapfile package\n
@@ -41,10 +41,10 @@ then
     #   can be set to empty for no limit, but beware of filled partitions!\n
     #   this is/was a (outdated?) 32bit kernel limit (in MBytes), do not overrun it\n
     #   but is also sensible on 64bit to prevent filling /var or even / partition\n
-    CONF_MAXSWAP=$SWAP" >> /etc/dphys-swapfile && \
+    #CONF_MAXSWAP=$SWAP" > /etc/dphys-swapfile && \
     sudo dphys-swapfile setup && \
     sudo dphys-swapfile swapon && \
-    echo "Swap limit to ${SWAP}MB was successful"
+    echo "Swap limit was set to default succesfully!"
 else
     sudo dphys-swapfile swapoff && \
     printf "# /etc/dphys-swapfile - user settings for dphys-swapfile package\n
@@ -62,7 +62,7 @@ else
     \n
     # set size to absolute value, leaving empty (default) then uses computed value\n
     #   you most likely don't want this, unless you have an special disk situation\n
-    CONF_SWAPSIZE=100\n
+    CONF_SWAPSIZE=$SWAP\n
     \n
     # set size to computed value, this times RAM size, dynamically adapts,\n
     #   guarantees that there is enough swap without wasting disk space on excess\n
@@ -72,8 +72,8 @@ else
     #   can be set to empty for no limit, but beware of filled partitions!\n
     #   this is/was a (outdated?) 32bit kernel limit (in MBytes), do not overrun it\n
     #   but is also sensible on 64bit to prevent filling /var or even / partition\n
-    #CONF_MAXSWAP=$SWAP" >> /etc/dphys-swapfile && \
+    CONF_MAXSWAP=$SWAP" > /etc/dphys-swapfile && \
     sudo dphys-swapfile setup && \
     sudo dphys-swapfile swapon && \
-    echo "Swap limit back to system default was successful"
+    echo "swap to ${SWAP}MB was successful"
  fi
