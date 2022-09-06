706a90e010250747e663dbd3623c8dfa9dcadd20
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-06 14:58:52 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-[[ ${1} -gt 4]] && SWAP=$((1024*4)) || SWAP=$(( 1024 * $1))
+if [ $1 -gt 4]; then
+    SWAP=$((1024*4)) 
+else
+ SWAP=$(( 1024 * $1))
+fi
 echo "Testing... swap size: $SWAP"
