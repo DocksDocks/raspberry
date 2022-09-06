2923523c35acdb83d0b54cc7496d47e6cfbe5b45
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-06 13:09:43 -0300

    Update swap_change.sh

--- swap_change.sh
+++ swap_change.sh
@@ -1,5 +1,32 @@
+SWAP=$(( 1024 * $1))
+
 sudo dphys-swapfile swapoff && \
-dd if=/dev/zero of=/"$1"swap bs=1M count=$((512*2*$1)) && \
-mkswap /"$1"swap && \
-swapon /"$1"swap && \
-echo "/""$1""swap          none  swap sw       0 0" >> /etc/fstab
+printf "# /etc/dphys-swapfile - user settings for dphys-swapfile package\n
+# author Neil Franklin, last modification 2010.05.05\n
+# copyright ETH Zuerich Physics Departement\n
+#   use under either modified/non-advertising BSD or GPL license\n
+\n
+# this file is sourced with . so full normal sh syntax applies\n
+\n
+# the default settings are added as commented out CONF_*=* lines\n
+\n
+\n
+# where we want the swapfile to be, this is the default\n
+#CONF_SWAPFILE=/var/swap\n
+\n
+# set size to absolute value, leaving empty (default) then uses computed value\n
+#   you most likely don't want this, unless you have an special disk situation\n
+CONF_SWAPSIZE=$SWAP\n
+\n
+# set size to computed value, this times RAM size, dynamically adapts,\n
+#   guarantees that there is enough swap without wasting disk space on excess\n
+#CONF_SWAPFACTOR=2\n
+\n
+# restrict size (computed and absolute!) to maximally this limit\n
+#   can be set to empty for no limit, but beware of filled partitions!\n
+#   this is/was a (outdated?) 32bit kernel limit (in MBytes), do not overrun it\n
+#   but is also sensible on 64bit to prevent filling /var or even / partition\n
+#CONF_MAXSWAP=$SWAP" >> /etc/dphys-swapfile && \
+sudo dphys-swapfile setup && \
+sudo dphys-swapfile swapon && \
+echo "swap to $1GB was successful"
