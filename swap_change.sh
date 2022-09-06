f1d61db576ff7a9e26f8d0cf3469a780890ba588
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-06 13:00:23 -0300

    Update swap_change.sh

--- swap_change.sh
+++ swap_change.sh
@@ -1,4 +1,5 @@
-dd if=/dev/zero of=/"$1"swap bs=1M count=1024*"$1" && \
+sudo dphys-swapfile swapoff && \
+dd if=/dev/zero of=/"$1"swap bs=1M count=$((512*2*$1)) && \
 mkswap /"$1"swap && \
 swapon /"$1"swap && \
 echo "/""$1""swap          none  swap sw       0 0" >> /etc/fstab
