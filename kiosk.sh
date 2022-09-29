9806adea194365fe03ea2fd9ca62527db00610db
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-29 15:35:50 -0300

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -21,8 +21,8 @@ xset -dpms
 
 unclutter -root &
 
-sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/$USER/.config/chromium/Default/Preferences
-sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/$USER/.config/chromium/Default/Preferences
+sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /home/\$USER/.config/chromium/Default/Preferences
+sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/' /home/\$USER/.config/chromium/Default/Preferences
 
 /usr/bin/chromium --noerrdialogs --disable-infobars --kiosk https://youtube.com &" > /home/pi/kiosk.sh
 
