ced1dec1f7599953dc7e886f95dccf5b36be278e
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-09-29 13:20:40 -0300

    Create kiosk.sh

---
+++ kiosk.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+printf 
+"
+#!/bin/bash
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+
+/usr/bin/chromium --noerrdialogs --disable-infobars --kiosk https://youtube.com &
+" > /home/pi/kiosk.sh && \
+chmod u+x /home/pi/kiosk.sh && \
+printf "
+[Unit]
+Description=Chromium Kiosk
+Wants=graphical.target
+After=graphical.target
+
+[Service]
+Environment=DISPLAY=:0.0
+Environment=XAUTHORITY=/home/pi/.Xauthority
+Type=simple
+ExecStart=/bin/bash /home/pi/kiosk.sh
+Restart=on-abort
+User=pi
+Group=pi
+
+[Install]
+WantedBy=graphical.target
+" > /lib/systemd/system/kiosk.service && \
+sudo systemctl enable kiosk.service
