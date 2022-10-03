98fc1052976a396ba407d53be9a8c4a7c2122dcd
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2022-10-03 10:07:32 -0300

    Update testkiosk.sh

--- testkiosk.sh
+++ testkiosk.sh
@@ -35,6 +35,6 @@ chromium-browser  --noerrdialogs --disable-infobars --kiosk --check-for-update-i
 " > $filenameA
 
 printf "#!bin/sh
-[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor" >> $filenameB
+[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor" >> $filenameB
 
 echo "type:\n source ~/.bash_profile \nand then:\n sudo reboot"
